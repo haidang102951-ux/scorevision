601aecea1469dccb22e8a0ad1a642c1e74d9b520
Score Vision <haidang102951@gmail.com>
2026-08-29 08:49:24 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,45 @@
-const CACHE_NAME = 'scorevision-v1';
+const CACHE_NAME = 'scorevision-v2';
 const ASSETS = [
-  './',
-  './index.html',
-  './du-doan.html',
-  './manifest.json'
+    './',
+    './index.html',
+    './du-doan.html',
+    './bang-xep-hang.html',
+    './gop-y.html',
+    './gioi-thieu.html',
+    './ket-qua.html',
+    './lich-dau.html',
+    './lien-he.html',
+    './livescore.html',
+    './thong-ke.html',
+    './tin-nong.html',
+    './manifest.json',
+    './ads.txt'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache =>
+            cache.addAll(ASSETS).then(() => self.skipWaiting())
+        )
+    );
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        ).then(() => self.clients.claim())
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        fetch(e.request)
+            .then(res => {
+                const resClone = res.clone();
+                caches.open(CACHE_NAME).then(cache => cache.put(e.request, resClone));
+                return res;
+            })
+            .catch(() => caches.match(e.request))
+    );
 });
