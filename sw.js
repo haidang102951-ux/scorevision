bb1dc32fd69b173eb69cd625b0bae98371ddbb9d
Score Vision <haidang102951@gmail.com>
2026-08-29 14:23:28 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ const ASSETS = [
     './livescore.html',
     './thong-ke.html',
     './tin-nong.html',
-    './tai-app.html',     // ✅ THÊM DÒNG NÀY LÀ ĐỦ
+    './tai-app.html',
     './manifest.json',
     './ads.txt'
 ];
